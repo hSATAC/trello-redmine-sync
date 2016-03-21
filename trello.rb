2ee2c256f9f147227a1cbe435c1278380f763885
hSATAC <hsatac@gmail.com>
2016-03-21 15:21:10 +0800

    Preserve appendix

--- trello.rb
+++ trello.rb
@@ -18,14 +18,21 @@ board = Trello::Board.find( Settings.trello.board_id )
 
 redmine = Redmine::Client.new( Settings.redmine.url, Settings.redmine.token )
 
+# (12) [xxx] #1234 desc
+# [   保留 ] #照抄 [sync]
 board.cards.each do |card|
-  if card.name.match(/^#(\d+)(.*)/)
-    issue = redmine.issues.find($1)
-    if issue && ($2.strip != issue.issue[:subject] || card.desc != desc_with_url(issue))
+  if card.name.match(/#(\d+)/)
+    issue      = redmine.issues.find($1)
+    components = card.name.split("##{issue.issue[:id]}")
+    appendix   = components[0]
+    title      = components[1]
+    if issue && (title.strip != issue.issue[:subject] || card.desc != desc_with_url(issue))
       puts "Updating ##{issue.issue[:id]} #{issue.issue[:subject]}"
-      card.name = "##{issue.issue[:id]} #{issue.issue[:subject]}"
+      card.name = "#{appendix} ##{issue.issue[:id]} #{issue.issue[:subject]}".strip
       card.desc = desc_with_url(issue)
       card.save
+    else
+      puts "Ignoring ##{issue.issue[:id]} #{issue.issue[:subject]}"
     end
   end
 end
